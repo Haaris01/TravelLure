39c697469a753e19c4235c23043c6736fd0aeae5
Muhammed Haris <mdharis00000@gmail.com>
2022-02-22 21:41:37 +0530

    _

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const MongoStore = require("connect-mongo");
-const dbUrl = 'mongodb://localhost:27017/YelpCamp';
-// process.env.DB_URL || 
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/YelpCamp';
+
 const secret = process.env.SECRET || 'thishouldbeasecret';
 mongoose.connect(dbUrl);
 const db = mongoose.connection;
