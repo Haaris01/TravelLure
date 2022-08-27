989e0641b230ceb72f9e10f9d9ec49e6e2755e17
Haaris01 <mdharis00000@gmail.com>
2022-08-27 17:27:28 +0530

    changed db url

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const MongoStore = require("connect-mongo");
 
-const dbUrl = 'mongodb://localhost:27017/YelpCamp';
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/YelpCamp";
 
-const secret = process.env.DB_URL || 'thishouldbeasecret';
+const secret = 'thishouldbeasecret';
 mongoose.connect(dbUrl);
 const db = mongoose.connection;
 
